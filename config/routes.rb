3fc37902f9d23c67580cc527fd760e388e608ade
Ana Perez <anaceci.perezmorales@gmail.com>
2022-09-30 15:44:38 -0500

    add test devise configuration - Rspec with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
   devise_for :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'pages#home'
